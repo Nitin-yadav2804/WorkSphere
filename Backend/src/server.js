ab9a15b3d19000af4a4216aa728bc23b35a65ebc
Nitin Yadav <nitinyadavsapla1@gmail.com>
2026-09-10 17:01:10 +0530

    Removed Extra console log

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,8 +2,6 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-console.log("JWT_SECRET loaded:", !!process.env.JWT_SECRET);
-
 const PORT = process.env.PORT || 3000;
 
 connectDB();
